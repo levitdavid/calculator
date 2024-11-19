9574baa1c200fb2236541bc9bcbf3c07787e4a62
levitdavid <davidlevit85@gmail.com>
2024-11-19 17:46:46 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 def lex(s):
     a = s.split()
-    d = {'один': ('N', 1), 'два': ('N', 2), 'три': ('N', 3), 'четыре': ('N', 4), 'пять': ('N', 5), 'шесть': ('N', 6),
+    listnubers= {'один': ('N', 1), 'два': ('N', 2), 'три': ('N', 3), 'четыре': ('N', 4), 'пять': ('N', 5), 'шесть': ('N', 6),
          'семь': ('N', 7), 'восемь': ('N', 8), 'девять': ('N', 9), 'десять': ('N', 10), 'одиннадцать': ('N', 11),
          'двенадцать': ('N', 12), 'тринадцать': ('N', 13), 'четырнадцать': ('N', 14), 'пятнадцать': ('N', 15),
          'шестнадцать': ('N', 16), 'семнадцать': ('N', 17), 'восемнадцать': ('N', 18), 'левятнадцать': ('N', 19),
@@ -10,35 +10,35 @@ def lex(s):
          'открывается': ('O', '('), 'закрывается': ('O', ')'), 'на': ('E', '')}
     r = []
     for x in a:
-        if x in d:
-            r.append(d[x])
+        if x in listnubers:
+            r.append(listnubers[x])
         else:
             r.append('Unknown')
     return r
 
 
-def synt(a):
+def syntacsis(a):
     r = []
-    numb = False
+    numbers = False
     n = 0
     for x in a:
         if x[0] == 'N':
-            numb = True
+            numbers = True
             n += x[1]
         else:
-            if numb:
+            if numbers:
                 r.append(n)
                 n = 0
-            numb = False
+            numbers= False
             if x[0] == 'O':
                 r.append(x[1])
-    if numb:
+    if numbers:
         r.append(n)
     return r
 
 
-def nums(n):
-    res = ''
+def numers(n):
+    result = ''
     names = ['ноль', 'один', 'два', 'три', 'четыре', 'пять', 'шесть', 'семь', 'восемь', 'девять', 'десять',
              'одиннадцать', 'двенадцать', 'тринадцать', 'четырнадцать', 'пятнадцать', 'шестнадцать', 'семнадцать',
              'восемнадцать', 'девятнадцать']
@@ -47,32 +47,32 @@ def nums(n):
     if abs(n) >= 100:
         return 'Переполнение'
     if n < 0:
-        res = 'минус '
+        result = 'минус '
         n = -n
     if n < 20:
-        res += names[n]
-        return res
+        result += names[n]
+        return result
     des = n // 10
     ed = n % 10
-    res += names2[des]
+    result += names2[des]
     if ed != 0:
-        res += ' ' + names[ed]
-    return res.strip()
+        result += ' ' + names[ed]
+    return result.strip()
 
 
 def calc(s):
     a = lex(s)
     if 'Unknown' in a:
         return 'Неизвестное слово'
-    b = synt(a)
+    b = syntacsis(a)
     s = ''
     for x in b: s += str(x)
     try:
         r = eval(s)
     except:
         return 'Ошибка в выражении'
-    return nums(r)
+    return numers(r)
 
 
-s = 'семь минус шесть плюс скобка десять умножить на два скобка'
+s = 'сто умножить на сто'
 print(calc(s))
